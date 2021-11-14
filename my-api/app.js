3d3a98f07947b38b5b704b0d78d3bf3657bf3762
rupomsoft <rupomplaystore@gmail.com>
2021-11-15 00:55:05 +0600

    Undefined Route Class 41
    Undefined Route Class 41

--- my-api/app.js
+++ my-api/app.js
@@ -1,8 +1,24 @@
 const express=require('express');
 const router = require("./src/routes/api");
 const app=new express();
+
+
 app.use("/api/v1",router);
 
+
+
+
+
+
+
+
+
+
+
+//Undefined Route
+app.use('*',(req,res)=>{
+    res.status(404).json({status:"fail",data:"Not found"})
+})
 
 module.exports=app;
 
