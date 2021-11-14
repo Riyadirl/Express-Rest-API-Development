8041f84882562588110e0fa9304505080dbc55b2
rupomsoft <rupomplaystore@gmail.com>
2021-11-15 01:25:28 +0600

    Security Middleware Implement class 42
    Security Middleware Implement class 42

--- my-api/app.js
+++ my-api/app.js
@@ -2,8 +2,35 @@ const express=require('express');
 const router = require("./src/routes/api");
 const app=new express();
 
+//Security Middleware Import
+const rateLimit= require('express-rate-limit')
+const helmet=require('helmet')
+const mongoSanitize=require('express-mongo-sanitize')
+const xss=require('xss-clean')
+const hpp=require('hpp')
+const cors=require('cors')
+
+
+//Security Middleware Implement
+app.use(cors())
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+
+
+
+
+
+
+
+
+
+
 
 app.use("/api/v1",router);
+
+
 
 
 
