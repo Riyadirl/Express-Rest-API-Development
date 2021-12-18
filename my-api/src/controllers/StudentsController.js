1e4f8db4315a4986396dc0269388274422e3e94a
rupomsoft <rupomplaystore@gmail.com>
2021-12-18 18:35:07 +0600

    mongooes create read update delete basic completed
    mongooes create read update delete basic completed

---
+++ my-api/src/controllers/StudentsController.js
@@ -0,0 +1,64 @@
+const StudentsModel=require('../models/StudentsModel')
+
+
+// CRUD
+
+// C=Create
+exports.InsertStudent=(req,res)=>{
+   let reqBody= req.body;
+   StudentsModel.create(reqBody,(err,data)=>{
+       if(err){
+           res.status(400).json({status:"fail",data:err})
+       }
+       else {
+           res.status(200).json({status:"success",data:data})
+       }
+   })
+}
+
+// R=Read
+exports.ReadStudent=(req,res)=>{
+    let Query={};
+    let Projection="Name Roll"
+    StudentsModel.find(Query,Projection,(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
+        }
+    })
+}
+
+
+// U=Update
+
+exports.UpdateStudent=(req,res)=>{
+    let id=req.params.id;
+    let QUERY={_id:id}
+    let reqBody= req.body;
+    StudentsModel.updateOne(QUERY,reqBody,(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
+        }
+    })
+}
+
+
+exports.DeleteStudent=(req,res)=>{
+    let id=req.params.id;
+    let QUERY={_id:id}
+    StudentsModel.remove(QUERY,(err,data)=>{
+        if(err){
+            res.status(400).json({status:"fail",data:err})
+        }
+        else {
+            res.status(200).json({status:"success",data:data})
+        }
+    })
+
+}
+
--- my-api/src/routes/api.js
+++ my-api/src/routes/api.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const HelloController = require("../controllers/HelloController");
+const StudentsController = require("../controllers/StudentsController");
+
 const router=  express.Router();
 
 
@@ -8,9 +10,10 @@ router.get("/hello-get",HelloController.HelloGet)
 router.post("/hello-post",HelloController.HelloPost)
 
 
+//Mongooes
+router.post("/InsertStudent",StudentsController.InsertStudent)
+router.get("/ReadStudent",StudentsController.ReadStudent)
+router.post("/UpdateStudent/:id",StudentsController.UpdateStudent)
+router.get("/DeleteStudent/:id",StudentsController.DeleteStudent)
 
 module.exports=router;
-
-
-
-
