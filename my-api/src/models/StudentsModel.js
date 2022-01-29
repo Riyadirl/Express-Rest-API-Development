ca208cdccb620d39d248b53bf937005660f07c63
rupomsoft <rupomplaystore@gmail.com>
2022-01-29 17:08:57 +0600

    cvcx
    xcv

--- my-api/src/models/StudentsModel.js
+++ my-api/src/models/StudentsModel.js
@@ -1,9 +1,8 @@
 const mongoose= require('mongoose')
 const DataSchema= mongoose.Schema({
-    Name:String,
-    Roll:String,
-    Class:String,
-    Remarks:{type:String,default:"No Remarks"}
+    Name:{type:String},
+    Roll:{type:Number},
+    Class:{type:String}
 },{versionKey:false})
 const  StudentsModel= mongoose.model('students',DataSchema);
 module.exports=StudentsModel;
--- my-api/src/routes/api.js
+++ my-api/src/routes/api.js
@@ -1,23 +1,12 @@
 const express=require('express');
-const HelloController = require("../controllers/HelloController");
 const StudentsController = require("../controllers/StudentsController");
 
 const router=  express.Router();
 
-// This is my first get routing
-router.get("/hello-get",HelloController.HelloGet)
-router.post("/hello-post",HelloController.HelloPost)
-
-
-//Mongooes
 router.post("/InsertStudent",StudentsController.InsertStudent)
 router.get("/ReadStudent",StudentsController.ReadStudent)
 router.post("/UpdateStudent/:id",StudentsController.UpdateStudent)
 router.get("/DeleteStudent/:id",StudentsController.DeleteStudent)
-
-
-
-
 
 
 module.exports=router;
