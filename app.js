d530c04109c78fa5740efd9dcda7c794861103f5
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-06-28 13:46:21 +0600

    fixed

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-// app.js manage all configuration
-
+// Require necessary modules
 const express = require("express");
 const router = require("./src/routes/api");
 const app = express();
 
-//security middleware import
+// Import security middleware
 const bodyParser = require('body-parser');
 const rateLimit = require("express-mongo-sanitize");
 const helmet = require("helmet");
@@ -14,31 +13,23 @@ const cors = require("cors");
 const hpp = require("hpp");
 const mongoose = require("mongoose");
 
-//security middleware implementation
+// Set up security middleware
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(helmet())
+app.use(helmet());
+app.use(bodyParser.json());
 
-// JSON Parser
-app.use(bodyParser.json())
-
-
-
-//requesting rate limit
-
+// Request rate limit
 const limiter = rateLimit({
-  windowMs: 15 * 1000, //15 minutes
-  max: 100, //limit each ip to 100 requests per window
+  windowMs: 15 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per window
 });
 
-
-//use limiter
+// Use the rate limiter middleware
 app.use(limiter);
 
-//mongodb database connection
-
+// MongoDB database connection
 const URI = "mongodb+srv://<username>:<password>@practice002.q7fqa2z.mongodb.net/mern";
 const options = {
   useNewUrlParser: true,
@@ -56,35 +47,36 @@ mongoose
     console.error("Error connecting to MongoDB:", error);
   });
 
-
-
 /*
 // server
 const port = process.env.PORT || 5000;
 
 // Connect to DB and start server
 mongoose
- .connect(process.env.DATABASE)
- .then(() => {
-   app.listen(port, () => {
-     console.log(`Server Running on port ${port}`);
-   });
- })
- .catch((err) => console.log(err));
+.connect(process.env.DATABASE)
+.then(() => {
+ app.listen(port, () => {
+   console.log(`Server Running on port ${port}`);
+ });
+})
+.catch((err) => console.log(err));
 
 
 
 */
 
 
-//routing
-mongodb: app.use("/api/v1", router);
 
-//undefined route
-//app.use("*", (req, res) => {
-// res.status(404).json(
-//{ status: "Fail", data: "not found" });
-//});
 
-//export app
+// Routing
+app.use("/api/v1", router);
+
+// Undefined route
+/*
+app.use("*", (req, res) => {
+  res.status(404).json({ status: "Fail", data: "not found" });
+});
+*/
+
+// Export the app
 module.exports = app;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// Require the necessary modules
 const app = require("./app");
 const dotenv = require("dotenv");
+
+// Load environment variables from the config.env file
 dotenv.config({ path: "./config.env" });
 
-
+// Start the server and listen on the specified port
 app.listen(process.env.PORT, function () {
-  console.log("success! this is from env " + process.env.PORT);
+  console.log("Server is running on port " + process.env.PORT);
 });
